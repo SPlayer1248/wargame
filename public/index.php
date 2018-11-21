d3074f673428608db5ed9b26c100119af3eb413a
Hoai Vu <hoaivu1224@gmail.com>
2018-11-21 23:30:53 +0700

    Khai bao duong dan trong file index.php, cau hinh file .htaccess

--- public/index.php
+++ public/index.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HP
- * Date: 11/21/2018
- * Time: 10:20 PM
- */
+
+//Khai bao duong dan cho trang web
+define('ROOT',dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
+define('VIEW', ROOT . 'app' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
+define('MODEL', ROOT . 'app' . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR);
+define('DATA', ROOT . 'app' . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR);
+define('CORE', ROOT . 'app' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR);
+define('CONTROLLER' , ROOT . 'app' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR);
+$module = [ROOT,APP,CORE,CONTROLLER,DATA];
+
+set_include_path(get_include_path().PATH_SEPARATOR.implode(PATH_SEPARATOR,$module));
+spl_autoload_register('spl_autoload',false);
+
+var_dump($_SERVER['REQUEST_URI']);
